7d2d2d500bc075878ecb5be9ec757e1a5b1936b9
王瑞东 <ruiong.wang@tsingdata.com>
2019-11-09 11:26:27 +0800

    # [443] 压缩字符串

---
+++ 443.压缩字符串.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=443 lang=python3
+#
+# [443] 压缩字符串
+#
+
+# @lc code=start
+
+
+class Solution:
+    def compress(self, chars: list) -> int:
+        res = []
+        # 处理chars最后一个元素
+        chars.append('^')
+        pre_char = chars[0]
+        count = 0
+        index = 0
+        for char in chars:
+
+            if char == pre_char:
+                count += 1
+            else:
+                chars[index] = pre_char
+                index += 1
+                # res.append(pre_char)
+                if count > 1:
+                    for s in str(count):
+                        chars[index] = s
+                        index += 1
+                        # res.append(s)
+                pre_char = char
+                count = 1
+        return index
+
+
+if __name__ == "__main__":
+    s = Solution()
+    chars = ["a", "b", "b", "b", "b", "b",
+             "b", "b", "b", "b", "b", "b", "b"]
+    s.compress(chars)
+    print(chars)
+# @lc code=end
