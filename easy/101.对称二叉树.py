193a368bd0aecc7138ca803d04780c48d0bf5256
王瑞东 <ruidong.wang@tsingdata.com>
2020-09-08 14:32:51 +0800

    # [101] 对称二叉树

---
+++ easy/101.对称二叉树.py
@@ -0,0 +1,91 @@
+#
+# @lc app=leetcode.cn id=101 lang=python3
+#
+# [101] 对称二叉树
+#
+# https://leetcode-cn.com/problems/symmetric-tree/description/
+#
+# algorithms
+# Easy (52.24%)
+# Likes:    1004
+# Dislikes: 0
+# Total Accepted:    203.5K
+# Total Submissions: 384.6K
+# Testcase Example:  '[1,2,2,3,4,4,3]'
+#
+# 给定一个二叉树，检查它是否是镜像对称的。
+#
+#
+#
+# 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+#
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   2
+# ⁠/ \ / \
+# 3  4 4  3
+#
+#
+#
+#
+# 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+#
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   2
+# ⁠  \   \
+# ⁠  3    3
+#
+#
+#
+#
+# 进阶：
+#
+# 你可以运用递归和迭代两种方法解决这个问题吗？
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# 1.怎么判断一棵树是不是对称二叉树？ 答案：如果所给根节点，为空，那么是对称。如果不为空的话，当他的左子树与右子树对称时，他对称
+
+# 2.那么怎么知道左子树与右子树对不对称呢？在这我直接叫为左树和右树 答案：如果左树的左孩子与右树的右孩子对称，左树的右孩子与右树的左孩子对称，那么这个左树和右树就对称。
+
+# 仔细读这句话，是不是有点绕？怎么感觉有一个功能A我想实现，但我去实现A的时候又要用到A实现后的功能呢？
+
+# 当你思考到这里的时候，递归点已经出现了： 递归点：我在尝试判断左树与右树对称的条件时，发现其跟两树的孩子的对称情况有关系。
+
+# 想到这里，你不必有太多疑问，上手去按思路写代码，函数A（左树，右树）功能是返回是否对称
+
+# def 函数A（左树，右树）： 左树节点值等于右树节点值 且 函数A（左树的左子树，右树的右子树），函数A（左树的右子树，右树的左子树）均为真 才返回真
+
+# 实现完毕。。。
+
+# 写着写着。。。你就发现你写出来了。。。。。。
+
+
+from operator import le
+
+
+class Solution:
+    def isSymmetric(self, root):
+
+        def check(node1, node2):
+            if not node1 and not node2:
+                return True
+            elif not node1 or not node2:
+                return False
+
+            if node1.val != node2.val:
+                return False
+            return check(node1.left, node2.right) and check(node1.right, node2.left)
+
+        return check(root, root)
+
+# @lc code=end
