6c6340da9a38e4259e1f8cee09ea21351fb5953c
王瑞东 <ruidong.wang@tsingdata.com>
2020-09-17 15:07:11 +0800

    # [110] 平衡二叉树

---
+++ easy/110.平衡二叉树.py
@@ -0,0 +1,73 @@
+#
+# @lc app=leetcode.cn id=110 lang=python3
+#
+# [110] 平衡二叉树
+#
+# https://leetcode-cn.com/problems/balanced-binary-tree/description/
+#
+# algorithms
+# Easy (52.09%)
+# Likes:    479
+# Dislikes: 0
+# Total Accepted:    134.8K
+# Total Submissions: 246.7K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# 给定一个二叉树，判断它是否是高度平衡的二叉树。
+#
+# 本题中，一棵高度平衡二叉树定义为：
+#
+#
+# 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+#
+#
+# 示例 1:
+#
+# 给定二叉树 [3,9,20,null,null,15,7]
+#
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+#
+# 返回 true 。
+#
+# 示例 2:
+#
+# 给定二叉树 [1,2,2,3,3,null,null,4,4]
+#
+# ⁠      1
+# ⁠     / \
+# ⁠    2   2
+# ⁠   / \
+# ⁠  3   3
+# ⁠ / \
+# ⁠4   4
+#
+#
+# 返回 false 。
+#
+#
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def height(node):
+            if not node:
+                return 0
+            return max(height(node.left), height(node.right))+1
+        if not root:
+            return True
+        return abs(height(root.left)-height(root.right) <= 1) and self.isBalanced(root.left) and self.isBalanced(root.right)
+# @lc code=end
