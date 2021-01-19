5513d7923622afb79a6cb075ccf000637668a223
王瑞东 <ruidong.wang@tsingdata.com>
2021-01-19 11:03:02 +0800

    # [145] 二叉树的后序遍历

---
+++ medium/145.二叉树的后序遍历.py
@@ -0,0 +1,57 @@
+#
+# @lc app=leetcode.cn id=145 lang=python3
+#
+# [145] 二叉树的后序遍历
+#
+# https://leetcode-cn.com/problems/binary-tree-postorder-traversal/description/
+#
+# algorithms
+# Medium (73.90%)
+# Likes:    506
+# Dislikes: 0
+# Total Accepted:    179.8K
+# Total Submissions: 243.3K
+# Testcase Example:  '[1,null,2,3]'
+#
+# 给定一个二叉树，返回它的 后序 遍历。
+#
+# 示例:
+#
+# 输入: [1,null,2,3]
+# ⁠  1
+# ⁠   \
+# ⁠    2
+# ⁠   /
+# ⁠  3
+#
+# 输出: [3,2,1]
+#
+# 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+#
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        self.helper(root, res)
+        return res
+
+    def helper(self, root, res):
+        if root:
+            self.helper(root.left, res)
+            self.helper(root.right, res)
+            res.append(root.val)
+
+# @lc code=end
