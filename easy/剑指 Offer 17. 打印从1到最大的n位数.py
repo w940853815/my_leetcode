5dd75489d3bb64ca90cd36269574e51b814200b9
王瑞东 <ruidong.wang@tsingdata.com>
2021-01-20 10:48:33 +0800

    剑指 Offer 17. 打印从1到最大的n位数

---
+++ easy/剑指 Offer 17. 打印从1到最大的n位数.py
@@ -0,0 +1,10 @@
+# 输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+from typing import List
+
+
+class Solution:
+    def printNumbers(self, n: int) -> List[int]:
+        res = []
+        for i in range(1, 10**n):
+            res.append(i)
+        return res
