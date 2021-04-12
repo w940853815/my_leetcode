60e42b4b690adc9b8693ab0a6b2fd30d5e055d5a
王瑞东 <ruidong.wang@tsingdata.com>
2021-04-12 18:08:54 +0800

    # [179] 最大数

--- medium/179.最大数.py
+++ medium/179.最大数.py
@@ -6,14 +6,14 @@
 # https://leetcode-cn.com/problems/largest-number/description/
 #
 # algorithms
-# Medium (37.69%)
-# Likes:    452
+# Medium (38.12%)
+# Likes:    622
 # Dislikes: 0
-# Total Accepted:    50.8K
-# Total Submissions: 134.5K
+# Total Accepted:    80.7K
+# Total Submissions: 202.9K
 # Testcase Example:  '[10,2]'
 #
-# 给定一组非负整数 nums，重新排列它们每个数字的顺序（每个数字不可拆分）使之组成一个最大的整数。
+# 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
 #
 # 注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
 #
@@ -56,21 +56,23 @@
 #
 #
 #
-
-# @lc code=start
 from typing import List
 
-
+# @lc code=start
 class Comp(str):
     def __lt__(x: str, y: str) -> bool:
-        return x+y > y+x
+        return x + y > y + x
 
 
 class Solution:
-
     def largestNumber(self, nums: List[int]) -> str:
         # python3 sorted删除了cmp关键字参数
-        res = ''.join(sorted(map(str, nums), key=Comp))
-        return 0 if res[0] == 0 else res
+        res = "".join(sorted(map(str, nums), key=Comp))
+        return "0" if res[0] == "0" else res
+
 
 # @lc code=end
+if __name__ == "__main__":
+    s = Solution()
+    res = s.largestNumber([0, 0])
+    print(res)
