06545664f153b64c202d04d005371782958fd8df
王瑞东 <ruidong.wang@tsingdata.com>
2021-05-14 21:26:44 +0800

    # [12] 整数转罗马数字2

--- medium/12.整数转罗马数字.py
+++ medium/12.整数转罗马数字.py
@@ -9,9 +9,22 @@
 
 class Solution:
     def intToRoman(self, num: int) -> str:
-        map = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
-               (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
-        res = ''
+        map = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
+        res = ""
         index = 0
         while True:
             if num >= map[index][0]:
@@ -21,6 +34,32 @@ class Solution:
                 index += 1
             if num == 0:
                 break
+        return res
+
+
+class Solution2:
+    def intToRoman(self, num: int) -> str:
+        hashmap = {
+            1000: "M",
+            900: "CM",
+            500: "D",
+            400: "CD",
+            100: "C",
+            90: "XC",
+            50: "L",
+            40: "XL",
+            10: "X",
+            9: "IX",
+            5: "V",
+            4: "IV",
+            1: "I",
+        }
+        res = ""
+        for key in hashmap:
+            if num // key != 0:
+                cout = num // key
+                res += hashmap[key] * cout
+                num = num % key
         return res
 
 
