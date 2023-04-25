531a6700369a870bb48b137849c2f1c8f3f01528
ruidong <940853815@qq.com>
2023-04-25 11:05:07 +0800

    # [977] 有序数组的平方

---
+++ easy/977.有序数组的平方.py
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode.cn id=977 lang=python3
+#
+# [977] 有序数组的平方
+#
+# https://leetcode.cn/problems/squares-of-a-sorted-array/description/
+#
+# algorithms
+# Easy (68.08%)
+# Likes:    791
+# Dislikes: 0
+# Total Accepted:    523.8K
+# Total Submissions: 769.5K
+# Testcase Example:  '[-4,-1,0,3,10]'
+#
+# 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+#
+#
+#
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：nums = [-4,-1,0,3,10]
+# 输出：[0,1,9,16,100]
+# 解释：平方后，数组变为 [16,1,0,9,100]
+# 排序后，数组变为 [0,1,9,16,100]
+#
+# 示例 2：
+#
+#
+# 输入：nums = [-7,-3,2,3,11]
+# 输出：[4,9,9,49,121]
+#
+#
+#
+#
+# 提示：
+#
+#
+# 1 10^4
+# -10^4
+# nums 已按 非递减顺序 排序
+#
+#
+#
+#
+# 进阶：
+#
+#
+# 请你设计时间复杂度为 O(n) 的算法解决本问题
+#
+#
+#
+from typing import List
+# @lc code=start
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res = []
+        for num in nums:
+            res.append(num*num)
+        res.sort()
+        return res
+# @lc code=end
