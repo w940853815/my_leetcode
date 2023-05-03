4743dbb597a0745fb78b3e045a7d33807a87f2b9
ruidong <940853815@qq.com>
2023-05-03 10:54:19 +0800

    # [503] 下一个更大元素 II

---
+++ medium/503.下一个更大元素-ii.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode.cn id=503 lang=python3
+#
+# [503] 下一个更大元素 II
+#
+# https://leetcode.cn/problems/next-greater-element-ii/description/
+#
+# algorithms
+# Medium (66.68%)
+# Likes:    802
+# Dislikes: 0
+# Total Accepted:    195.9K
+# Total Submissions: 293.7K
+# Testcase Example:  '[1,2,1]'
+#
+# 给定一个循环数组 nums （ nums[nums.length - 1] 的下一个元素是 nums[0] ），返回 nums 中每个元素的
+# 下一个更大元素 。
+#
+# 数字 x 的 下一个更大的元素 是按数组遍历顺序，这个数字之后的第一个比它更大的数，这意味着你应该循环地搜索它的下一个更大的数。如果不存在，则输出 -1
+# 。
+#
+#
+#
+# 示例 1:
+#
+#
+# 输入: nums = [1,2,1]
+# 输出: [2,-1,2]
+# 解释: 第一个 1 的下一个更大的数是 2；
+# 数字 2 找不到下一个更大的数；
+# 第二个 1 的下一个最大的数需要循环搜索，结果也是 2。
+#
+#
+# 示例 2:
+#
+#
+# 输入: nums = [1,2,3,4,3]
+# 输出: [2,3,4,-1,4]
+#
+#
+#
+#
+# 提示:
+#
+#
+# 1 <= nums.length <= 10^4
+# -10^9 <= nums[i] <= 10^9
+#
+#
+#
+from typing import List
+# @lc code=start
+
+
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        stack = []
+        ans = [0]*n
+        for i in range(2*n-1, -1, -1):
+            while stack and stack[-1] <= nums[i % n]:
+                stack.pop()
+            ans[i % n] = stack[-1] if stack else -1
+            stack.append(nums[i % n])
+        return ans
+
+# @lc code=end
